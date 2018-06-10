c9ac22b4758abd6c55d1faefb9181094c263116f
Sudipta Sarkar <sudipta.javadev@gmail.com>
2018-06-10 23:31:55 +0530

    Completed interfae theme and other issues

--- onlineshoping/src/main/java/com/spring/onlineshoping/controller/PageController.java
+++ onlineshoping/src/main/java/com/spring/onlineshoping/controller/PageController.java
@@ -3,6 +3,7 @@ package com.spring.onlineshoping.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -11,11 +12,28 @@ public class PageController {
 	@RequestMapping(value = {"/", "/home", "/index"})
 	public ModelAndView index() {		
 		ModelAndView mv = new ModelAndView("page");		
-		mv.addObject("greeting","Welcome to Spring web MVC");
+		mv.addObject("title","Home");
+		mv.addObject("userClickHome",true);
 		return mv;				
 	}
 	
-/*	@RequestMapping(value="/test")
+	@RequestMapping(value ="/about")
+	public ModelAndView about() {		
+		ModelAndView mv = new ModelAndView("page");		
+		mv.addObject("title","About Us");
+		mv.addObject("userClickAbout",true);
+		return mv;				
+	}
+	
+	@RequestMapping(value ="/contact")
+	public ModelAndView contact() {		
+		ModelAndView mv = new ModelAndView("page");		
+		mv.addObject("title","Contact Us");
+		mv.addObject("userClickContact",true);
+		return mv;				
+	}
+	
+	/*@RequestMapping(value="/test")
 	public ModelAndView test(@RequestParam(value="greeting",required=false) String greeting) {
 		if(greeting==null) {
 			greeting="Hello There";
@@ -29,6 +47,13 @@ public class PageController {
 		
 		ModelAndView mv = new ModelAndView("page");		
 		mv.addObject("greeting",greeting);
+		return mv;
+	}*/
+	/*@RequestMapping(value="/test")
+	public  ModelAndView test() {
+	
+		ModelAndView mv = new ModelAndView("page");		
+		mv.addObject("greeting","Hello There!!!");
 		return mv;
 	}*/
 
